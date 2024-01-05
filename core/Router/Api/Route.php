eb1192826b59816d9066fc369e94caced447f806
mason.hows11 <mason.hows11@gmail.com>
2024-01-05 22:21:25 +0330

    updated

--- core/Router/Api/Route.php
+++ core/Router/Api/Route.php
@@ -6,24 +6,7 @@ namespace Core\Router\Api;
 
 class Route
 {
-    public static function get($route, array $Context){
 
-        $controller = $Context[0];
-        $controller = explode("\\",$controller);
-        $controller = array_splice($controller,-1)[0];
-        $method = $Context[1];
-        global $routes;
-        array_push($routes['get'] , array('route' => 'api/'.$route , 'controller' => $controller, 'method' => $method));
-
-    }
-    public static function post($route, array $Context){
-        $controller = $Context[0];
-        $controller = explode("\\",$controller);
-        $controller = array_splice($controller,-1)[0];
-        $method = $Context[1];
-        global $routes;
-        array_push($routes['post'] , array('route' => 'api/'.$route , 'controller' => $controller, 'method' => $method));
-    }
 
 
 
