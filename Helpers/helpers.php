31770283740e153dbff5f420b3e0413579089631
mason.hows11 <mason.hows11@gmail.com>
2024-01-06 21:55:37 +0330

    add assets config & files

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 function site_url($route)  
 {
     return $_ENV['BASE_URL'] . $route;
@@ -13,8 +10,10 @@ function asset_url($route)
     return site_url("assets/".$route);
 }
 
+function random_element($array){
+    shuffle($array);
+    return array_pop($array);
+}
 
 
 
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,22 +1,20 @@
 <?php
 
  include "../bootstrap/bootstrap.php";
- 
 
 
 
-echo site_url("product/index");
-
-////// 
-
+//////
 // for add autoload php  for load all class & files
 // base on namespaces
 // ../ this mean return one step back in project path
 
 
 //////
-
 // for test composer autoload
 // use App\Core\Request;
 // echo $_SERVER['REQUEST_URI'] . "\n";
 // $req = new Request()
+
+
+
