ac27bdf29bf1e48220e71fe8270cbb52ddb2ccf7
mason.hows11 <mason.hows11@gmail.com>
2024-01-07 17:03:15 +0330

    update simple route enginee

--- app/Core/Router/Router.php
+++ app/Core/Router/Router.php
@@ -1,1 +1,21 @@
 <?php
+
+
+namespace App\Core\Router;
+
+
+class Router
+{
+
+    private $routes;
+
+    public function __construct()
+    {
+        $this->routes = [
+            'pages/page_one' => 'pages/page_one.php',
+            'pages/page_tow' => 'pages/page_two.php',
+            'pages/page_three' => 'pages/page_three.php',
+            ];
+    }
+
+}
