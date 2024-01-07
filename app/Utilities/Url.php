39dafddda36a9cc3ce1dad5649cac396b73f8f6d
mason.hows11 <mason.hows11@gmail.com>
2024-01-07 16:40:15 +0330

    add current route method

--- app/Utilities/Url.php
+++ app/Utilities/Url.php
@@ -9,16 +9,17 @@ class Url
 
     public static function currentUrl()
     {
-        return  (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    }
-    public static function current_route()
-    {
-        return  $_SERVER["REQUEST_URI"];
+        return (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }
 
-        //    public static function segmentUrl()
-        //    {
-        //        return  (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        //    }
+    public static function current_route()
+    {
+        return strtok($_SERVER['REQUEST_URI'], '?');
+    }
+
+    //    public static function segmentUrl()
+    //    {
+    //        return  (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    //    }
 
 }
