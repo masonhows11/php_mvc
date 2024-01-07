88296398c96e9216c7dda0adf3bbc23bea4e7f1d
mason.hows11 <mason.hows11@gmail.com>
2024-01-07 17:37:15 +0330

    update simple route engine updated

--- app/Core/Router/Router.php
+++ app/Core/Router/Router.php
@@ -4,6 +4,8 @@
 namespace App\Core\Router;
 
 
+use App\Utilities\Url;
+
 class Router
 {
 
@@ -12,10 +14,29 @@ class Router
     public function __construct()
     {
         $this->routes = [
-            'pages/page_one' => 'pages/page_one.php',
-            'pages/page_tow' => 'pages/page_two.php',
-            'pages/page_three' => 'pages/page_three.php',
-            ];
+            '/pages/page_one' => 'page_one.php',
+            '/pages/page_two' => 'page_two.php',
+            '/pages/page_three' => 'page_three.php',
+        ];
     }
 
+
+    public function run()
+    {
+        try {
+            $current_route = Url::current_route();
+
+            foreach ($this->routes as $route => $view) {
+                if($current_route == $route)
+                {
+                    include BASE_PATH .  "views/pages/$view";
+                    die();
+                }
+            }
+        }catch (\Exception $ex){
+
+            echo $ex->getMessage();
+        }
+
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Core\Router\Router;
+
 include "../bootstrap/bootstrap.php";
 
-//////
-// for add autoload php  for load all class & files
-// base on namespaces
-// ../ this mean return one step back in project path
 
+// echo \App\Utilities\Url::current_route();
 
-//////
-// for test composer autoload
-// use App\Core\Request;
-// echo $_SERVER['REQUEST_URI'] . "\n";
-// $req = new Request()
-
-
-
+$router = new Router();
+$router->run();
