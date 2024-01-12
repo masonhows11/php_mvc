94137a458a20817b4d05cf8bbc374d147aa1c332
mason.hows11 <mason.hows11@gmail.com>
2024-01-12 21:43:22 +0330

    add nice_dump helper

--- public/index.php
+++ public/index.php
@@ -4,7 +4,17 @@ include "../bootstrap/bootstrap.php";
 
 //$router = new \App\Core\Routing\Router();
 //$router->run();
-
+$route_pattern = '/^\/post\/(?<slug>[-%\w]+)$/';
 
 $route = '/post/{slug}';
 $uri1 = '/post/what-is-php-language-programming';
+$uri2 = '/post/why-u-need-learn-programming';
+$uri3 = '/product/iphone-15-black';
+
+$result1 = preg_match($route_pattern,$uri1,$matches);
+nice_dump($result1);
+nice_dump($matches);
+
+$result2 = preg_match($route_pattern,$uri2,$matches2);
+nice_dump($result2);
+nice_dump($matches2);
