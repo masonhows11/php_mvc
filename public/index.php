eae60700fe25cf65e50c28753552f287b13e9e8d
mason.hows11 <mason.hows11@gmail.com>
2024-01-13 17:48:40 +0330

    implement pattern on route

--- public/index.php
+++ public/index.php
@@ -2,19 +2,13 @@
 
 include "../bootstrap/bootstrap.php";
 
-//$router = new \App\Core\Routing\Router();
-//$router->run();
-$route_pattern = '/^\/post\/(?<slug>[-%\w]+)$/';
+// $router = new \App\Core\Routing\Router();
+// $router->run();
+// $route_pattern = '/^\/post\/(?<slug>[-%\w]+)$/';
 
 $route = '/post/{slug}';
-$uri1 = '/post/what-is-php-language-programming';
-$uri2 = '/post/why-u-need-learn-programming';
-$uri3 = '/product/iphone-15-black';
+$pattern = "/^".str_replace(['/','{','}'],['\/','(?<','>[-%\w]+)'],$route)."$/";
 
-$result1 = preg_match($route_pattern,$uri1,$matches);
-nice_dump($result1);
-nice_dump($matches);
-
-$result2 = preg_match($route_pattern,$uri2,$matches2);
-nice_dump($result2);
-nice_dump($matches2);
+nice_dump($route);
+nice_dump($pattern);
+nice_dump('/^\/post\/(?<slug>[-%\w]+)$/');
